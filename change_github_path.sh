3934da125fa7ef7860c860faa308f06188cbd695
Harry Stern <harry@harrystern.net>
2022-02-22 16:25:31 -0500

    And show difference with mkdir

--- change_github_path.sh
+++ change_github_path.sh
@@ -7,6 +7,10 @@ chmod o+rx $my_tmp_dir
 echo "after chmod"
 ls -ld $my_tmp_dir
 
+mkdir /tmp/other_dir
+echo "check mkdir permissions"
+ls -ld /tmp/other_dir
+
 touch $my_tmp_dir/hello
 chmod +x $my_tmp_dir/hello
 
