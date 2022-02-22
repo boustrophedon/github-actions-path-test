374acf253aacd62b57f422c121211280c3679632
Harry Stern <harry@harrystern.net>
2022-02-22 16:22:50 -0500

    Fix with chmod?

--- change_github_path.sh
+++ change_github_path.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 my_tmp_dir="$(mktemp -d --tmpdir mytmpdir.XXXXX)"
+echo "before chmod"
+ls -ld $my_tmp_dir
+chmod o+rx $my_tmp_dir
+echo "after chmod"
+ls -ld $my_tmp_dir
+
 touch $my_tmp_dir/hello
 chmod +x $my_tmp_dir/hello
 
